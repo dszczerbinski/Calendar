7e7b95612122b959884883b27dbdac5d342d7883
Daniel <daniels@biznesport.pl>
2021-02-18 12:23:16 +0100

    Added PayU as payment method
    Cleanup code

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -6,13 +6,15 @@ use App\Services\GoogleApiService;
 use Doctrine\ORM\EntityManagerInterface;
 use Google_Client;
 use Google_Service_Calendar;
+use OpenPayU_Order;
+use OpenPayU_Util;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Constraints\Date;
 
 
-require_once 'C:\composer\calendar\vendor\autoload.php';
+require_once realpath(dirname(__FILE__, 3)). '/vendor/autoload.php';        //'C:\composer\calendar\vendor\autoload.php';
 
 class CalendarController extends AbstractController
 {
@@ -31,9 +33,51 @@ class CalendarController extends AbstractController
      */
     public function calendar(): Response
     {
-        $events=$this->googleAPI->createGoogle();
+        $googleService = $this->googleAPI->createGoogle();
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $eventsList = $this->googleAPI->getEventList($googleService);
+
+        if (isset($_POST['register'])) {
+            //session_start();
+            $_SESSION["registered"] = "yes";
+
+            $response = $this->googleAPI->createPayUEvent();
+            return $this->redirect($response->redirectUri);
+        }
         return $this->render('calendar.html.twig', [
-            'events' => $events,
-            ]);
+            'events' => $eventsList,
+            'ip' => $ip,
+        ]);
+    }
+
+    /**
+     * @Route("/orderstatus")
+     */
+    public function success(): Response
+    {
+        session_start();
+        if(isset($_SESSION["registered"])){
+            if(isset($_GET['error'])){
+                echo  nl2br ("Transakcja nieudana, spróbuj ponownie! \n Przekierowanie na strone główna nastąpi za 5 sekund...");
+                session_unset();
+                header( "refresh:5;url=https://kalendarz.biznesport.com.pl" );
+            }
+            else{
+                $googleService = $this->googleAPI->createGoogle();
+                $eventsList = $this->googleAPI->getEventList($googleService);
+                $this->googleAPI->createGoogleEvent($googleService);
+                echo  nl2br ("Transakcja udana! \n 
+            Jeśli posiadasz Kalendarz Google to wizyta została automatycznie dodana 
+            do twojego kalendarza i otrzymasz powiadomienie dzień przed o umówionej wizycie \n
+            Przekierowanie na strone główna nastąpi za 5 sekund...");
+            }
+            session_unset();
+            header( "refresh:5;url=https://kalendarz.biznesport.com.pl" );
+        }
+        else {
+            echo "Nie zarejestrowano żadnej transakcji, wypełnij formularz jeszcze raz i opłać wizytę";
+        }
+
+        return $this->render('base.html.twig');
     }
 }
--- src/Services/GoogleApiService.php
+++ src/Services/GoogleApiService.php
@@ -5,16 +5,25 @@ namespace App\Services;
 
 use Google_Client;
 use Google_Service_Calendar;
+use OpenPayU_Configuration;
+use OpenPayU_Exception;
+use OpenPayU_Order;
+use OpenPayU_Util;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 class GoogleApiService
 {
     public function createGoogle()
     {
+        if(!isset($_SESSION)){
+            session_start();
+        }
         $client = new Google_Client();
         $client->setApplicationName('Calendar');
         $client->addScope('https://www.googleapis.com/auth/calendar');
-        $client->setAuthConfig('C:\composer\calendar\credentials.json');
+        $client->setAuthConfig(realpath(dirname(__FILE__, 3)). '/credentials.json'); //'C:\composer\calendar\credentials.json');
         $client->setAccessType('offline');
         $client->setPrompt('select_account consent');
 
@@ -22,7 +31,7 @@ class GoogleApiService
         // The file token.json stores the user's access and refresh tokens, and is
         // created automatically when the authorization flow completes for the first
         // time.
-        $tokenPath = 'C:\composer\calendar\token.json';
+        $tokenPath = realpath(dirname(__FILE__, 3)). '/token.json';
         if (file_exists($tokenPath)) {
             $accessToken = json_decode(file_get_contents($tokenPath), true);
             $client->setAccessToken($accessToken);
@@ -58,7 +67,11 @@ class GoogleApiService
         // Get the API client and construct the service object.
         $service = new Google_Service_Calendar($client);
 
-// Print the next 10 events on the user's calendar.
+        return $service;
+    }
+
+    public function getEventList(Google_Service_Calendar $service) {
+        // Print the next 10 events on the user's calendar.
         $calendarId = 'daniels@biznesport.pl';
         $optParams = array(
             'maxResults' => 10,
@@ -69,52 +82,93 @@ class GoogleApiService
         $results = $service->events->listEvents($calendarId, $optParams);
         $events = $results->getItems();
 
-        if (isset($_POST['register'])) {
-            $clientemail = $_POST['clientemail'];
-            $date = $_POST['date'];
-            $specialist = $_POST['specialist'];
-            $nameSurname = $_POST['nameSurname'];
-            $phoneNumber = $_POST['phoneNumber'];
-            $starttime = $_POST['time'];
-            $startEvent = $date . "T" . $starttime . ":00+01:00";
-            $endEvent = $startEvent;
-            $endEvent[12] = intval($endEvent[12]) + 1;
-
-
-            $event = new \Google_Service_Calendar_Event(array(
-                'summary' => 'Termin zajęty',
-                'location' => 'Gabinet ul. Wojska Polskiego',
-                'description' => 'Wizyta u specjalisty ' . $specialist . '
-Imię i nazwisko klienta: ' . $nameSurname . '
-Numer telefonu klienta: ' . $phoneNumber . '
-Email klienta: ' . $clientemail,
-
-                'start' => array(
-                    'dateTime' => $startEvent,
-                    'timeZone' => 'Europe/Warsaw',
-                ),
-                'end' => array(
-                    'dateTime' => $endEvent,
-                    'timeZone' => 'Europe/Warsaw',
-                ),
-                'attendees' => array(
-                    array('email' => 'daniels@biznesport.pl'),
-                    array('email' => 'danielspraktyka@gmail.com'),
-                    array('email' => $clientemail),
-                ),
-//                'reminders' => array(
-//                    'useDefault' => FALSE,
-//                    'overrides' => array(
-//                        array('method' => 'email', 'minutes' => 24 * 60),
-//                        array('method' => 'popup', 'minutes' => 10),
-//                    ),
-//                ),
-            ));
-
-            $calendarId = 'daniels@biznesport.pl';
-            $event = $service->events->insert($calendarId, $event);
-            header("Refresh:0");
-        }
         return $events;
+    }
+
+    public function createPayUEvent() {
+        $_SESSION["clientemail"] = $_POST['clientemail'];
+        $_SESSION["date"] = $_POST['date'];
+        $_SESSION["specialist"] = $_POST['specialist'];
+        $_SESSION["nameSurname"] = $_POST['nameSurname'];
+        $_SESSION["phoneNumber"] = $_POST['phoneNumber'];
+        $_SESSION["starttime"] = $_POST['time'];
+        $_SESSION["startEvent"] = $_SESSION["date"] . "T" . $_SESSION["starttime"] . ":00+01:00";
+        $_SESSION["endEvent"] = $_SESSION["startEvent"];
+        $_SESSION["endEvent"][12] = intval($_SESSION["endEvent"][12]) + 1;
+
+
+        /// PAYU ///
+        ///
+        OpenPayU_Configuration::setEnvironment('sandbox');
+
+        //set POS ID and Second MD5 Key (from merchant admin panel)
+        OpenPayU_Configuration::setMerchantPosId('403458');
+        OpenPayU_Configuration::setSignatureKey('12e86e6c9c643dcb05b98fa1b0260906');
+
+        //set Oauth Client Id and Oauth Client Secret (from merchant admin panel)
+        OpenPayU_Configuration::setOauthClientId('403458');
+        OpenPayU_Configuration::setOauthClientSecret('bacb02b7f09ba32ca6bd26997acfb2fd');
+
+        $pieces = explode(" ", $_SESSION["nameSurname"]);
+
+
+        $order = array();
+
+        $order['notifyUrl'] = 'https://kalendarz.biznesport.com.pl/orderstatus';
+        $order['continueUrl'] = 'https://kalendarz.biznesport.com.pl/orderstatus';
+
+        $order['customerIp'] = $_SERVER['REMOTE_ADDR'];
+        $order['merchantPosId'] = OpenPayU_Configuration::getOauthClientId() ? OpenPayU_Configuration::getOauthClientId() : OpenPayU_Configuration::getMerchantPosId();
+        $order['description'] = 'Wizyta w gabinecie';
+        $order['currencyCode'] = 'PLN';
+        $order['totalAmount'] = 1000;
+        $order['extOrderId'] = uniqid('', true);
+
+        $order['products'][0]['name'] = 'Wizyta';
+        $order['products'][0]['unitPrice'] = 1000;
+        $order['products'][0]['quantity'] = 1;
+
+        $order['buyer']['email'] = $_SESSION["clientemail"];
+        $order['buyer']['phone'] = $_SESSION["phoneNumber"];
+        $order['buyer']['firstName'] = $pieces[0];
+        $order['buyer']['lastName'] = $pieces[1];
+        $order['buyer']['language'] = 'pl';
+
+        $response = OpenPayU_Order::create($order);
+
+        return $response->getResponse();
+    }
+    public function createGoogleEvent($service){
+        $event = new \Google_Service_Calendar_Event(array(
+            'summary' => 'Termin zajęty',
+            'location' => 'Gabinet ul. Wojska Polskiego',
+            'description' => 'Wizyta u specjalisty ' . $_SESSION["specialist"] . '
+Imię i nazwisko klienta: ' . $_SESSION["nameSurname"] . '
+Numer telefonu klienta: ' . $_SESSION["phoneNumber"] . '
+Email klienta: ' . $_SESSION["clientemail"],
+
+            'start' => array(
+                'dateTime' => $_SESSION["startEvent"],
+                'timeZone' => 'Europe/Warsaw',
+            ),
+            'end' => array(
+                'dateTime' => $_SESSION["endEvent"],
+                'timeZone' => 'Europe/Warsaw',
+            ),
+            'attendees' => array(
+                array('email' => 'daniels@biznesport.pl'),
+                array('email' => 'danielspraktyka@gmail.com'),
+                array('email' => $_SESSION["clientemail"]),
+            ),
+            'reminders' => array(
+                'useDefault' => FALSE,
+                'overrides' => array(
+                    array('method' => 'email', 'minutes' => 24 * 60),
+                    array('method' => 'popup', 'minutes' => 24 * 60),
+                ),
+            ),
+        ));
+        $calendarId = 'daniels@biznesport.pl';
+        $event = $service->events->insert($calendarId, $event);
     }
 }
